b3a312a729bfff47125617ffc51ab4483d4e0004
adityarrestu <adit.restu77@gmail.com>
2023-07-20 11:15:32 +0700

    add auth controller and API route

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register (Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required | email | unique:users, email',
+            'password' => 'required | min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        $token = $user->generateToken();
+
+        return response()->json([
+            'message' => 'User registered successfull',
+            'token' => $token,
+        ], 200);
+    }
+
+    public function login (Request $request)
+    {
+        $request ->validate([
+
+        ]);
+    }
+
+    public function logout(Request $request) 
+    {
+        $request->user()->token()->revoke();
+        
+        return response()->json(['message' => 'Logout successful'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
@@ -14,9 +15,13 @@ use App\Http\Controllers\ItemController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+// });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 
 Route::get('/items', [ItemController::class, 'index']);
 Route::post('/items', [ItemController::class, 'store']);
